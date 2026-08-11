8b253aad2c9744c944ebe853bd289aa458f3162e
entityname <mcetverov26@gmail.com>
2026-08-11 17:45:57 +0400

    too noisy, removed

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -5,5 +5,5 @@ wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip 
 unzip android-ndk-r10e-linux-x86_64.zip > /dev/null 2>&1
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export NDK_HOME=$PWD/android-ndk-r10e/
-./waf configure -T release --android=aarch64,4.9,32 -vvv --togles --disable-warns --enable-speex --enable-opus --build-games=cstrike --prefix=./android_aarch64_build &&
+./waf configure -T release --android=aarch64,4.9,32 --togles --disable-warns --enable-speex --enable-opus --build-games=cstrike --prefix=./android_aarch64_build &&
 ./waf install
