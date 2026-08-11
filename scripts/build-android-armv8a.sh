ab27af5538fe96cfa29416b99684fef264fc5972
entityname <mcetverov26@gmail.com>
2026-08-11 17:04:14 +0400

    update (2)

--- scripts/build-android-armv8a.sh
+++ scripts/build-android-armv8a.sh
@@ -5,5 +5,5 @@ wget https://dl.google.com/android/repository/android-ndk-r25c-linux.zip -o /dev
 unzip android-ndk-r25c-linux.zip
 export ANDROID_NDK_HOME=$PWD/android-ndk-r25c/
 export NDK_HOME=$PWD/android-ndk-r25c/
-./waf configure -T release --android=arm64-v8a-hard,4.9,32 --togles --disable-warns --enable-speex --enable-opus --build-games=cstrike --prefix=./android_armv8a_build &&
+./waf configure -T release --android=aarch64,4.9,32 --togles --disable-warns --enable-speex --enable-opus --build-games=cstrike --prefix=./android_armv8a_build &&
 ./waf install
