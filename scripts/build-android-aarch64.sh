32c2ed77ff97b7f9b0660b93cc0a185d12a48c31
entityname <mcetverov26@gmail.com>
2026-08-11 17:33:18 +0400

    changed verbosity level, and removed unzip log spam

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -2,8 +2,8 @@
 
 git submodule init && git submodule update
 wget https://dl.google.com/android/repository/android-ndk-r20b-linux-x86_64.zip -o /dev/null
-unzip android-ndk-r20b-linux-x86_64.zip
+unzip android-ndk-r20b-linux-x86_64.zip > /dev/null 2>&1
 export ANDROID_NDK_HOME=$PWD/android-ndk-r20b/
 export NDK_HOME=$PWD/android-ndk-r20b/
-./waf configure -T release --android=aarch64,4.9,32 -v --togles --disable-warns --enable-speex --enable-opus --build-games=cstrike --prefix=./android_aarch64_build &&
+./waf configure -T release --android=aarch64,4.9,32 -vvv --togles --disable-warns --enable-speex --enable-opus --build-games=cstrike --prefix=./android_aarch64_build &&
 ./waf install
