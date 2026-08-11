2e402aa6cba3107efef7b2aacd608c51265a435b
entityname <mcetverov26@gmail.com>
2026-08-11 17:43:34 +0400

    test

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 git submodule init && git submodule update
-wget https://dl.google.com/android/repository/android-ndk-r20b-linux-x86_64.zip -o /dev/null
-unzip android-ndk-r20b-linux-x86_64.zip > /dev/null 2>&1
-export ANDROID_NDK_HOME=$PWD/android-ndk-r20b/
-export NDK_HOME=$PWD/android-ndk-r20b/
+wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
+unzip android-ndk-r10e-linux-x86_64.zip > /dev/null 2>&1
+export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
+export NDK_HOME=$PWD/android-ndk-r10e/
 ./waf configure -T release --android=aarch64,4.9,32 -vvv --togles --disable-warns --enable-speex --enable-opus --build-games=cstrike --prefix=./android_aarch64_build &&
 ./waf install
