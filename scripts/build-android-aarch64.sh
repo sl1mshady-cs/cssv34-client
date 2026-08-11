b1818f6657b2b252c1e6b5019066a88186430f5d
entityname <mcetverov26@gmail.com>
2026-08-11 17:24:35 +0400

    added verbosity for arm64-v8a build script

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -5,5 +5,5 @@ wget https://dl.google.com/android/repository/android-ndk-r20b-linux-x86_64.zip 
 unzip android-ndk-r20b-linux-x86_64.zip
 export ANDROID_NDK_HOME=$PWD/android-ndk-r20b/
 export NDK_HOME=$PWD/android-ndk-r20b/
-./waf configure -T release --android=aarch64,4.9,32 --togles --disable-warns --enable-speex --enable-opus --build-games=cstrike --prefix=./android_aarch64_build &&
+./waf configure -T release --android=aarch64,4.9,32 -v --togles --disable-warns --enable-speex --enable-opus --build-games=cstrike --prefix=./android_aarch64_build &&
 ./waf install
